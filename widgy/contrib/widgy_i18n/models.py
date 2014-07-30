c2bfc9a619913e52581a44dbd418a58a6865ca63
Johannes Ylönen <johannes@indiumgames.fi>
2014-07-31 00:26:00 +0300

    Added Copyright notice to license for covering code of Indiu Technology Oy and cleaned up some old code.

--- widgy/contrib/widgy_i18n/models.py
+++ widgy/contrib/widgy_i18n/models.py
@@ -3,12 +3,19 @@ from django.utils.translation import (
     ugettext_lazy as _, ugettext, get_language, get_language_info
 )
 from django.conf import settings
+<<<<<<< HEAD
  
+=======
+
+>>>>>>> 237843e... Added Copyright to license for covering code of Indiu Technology Oy and cleaned up some old code.
 from widgy.models import Content
 from widgy.contrib.page_builder.models import Layout
 from widgy.models.mixins import TabbedContainer, StrictDefaultChildrenMixin
 from widgy import register
+<<<<<<< HEAD
 
+=======
+>>>>>>> 237843e... Added Copyright to license for covering code of Indiu Technology Oy and cleaned up some old code.
 
 class I18NChild(models.Model):
     """
@@ -97,103 +104,4 @@ class I18NBucket(I18NChild, Content):
 
     	class Meta:
         	abstract = True
-"""
-@register
-class MainContentArea(Bucket):
-    	def valid_parent_of(self, cls, obj=None):
-        	return not issubclass(cls, (MainContentArea, SidebarArea))
 
-    	@classmethod
-    	def valid_child_of(cls, parent, obj=None):
-        	return isinstance(parent, I18NLayout)
-
-    	class Meta:
-        	verbose_name = _('main content')
-        	verbose_name_plural = _('main contents')
- 
-
-@register
-class SidebarArea(Bucket):
-    	pop_out = 1
-
-	def valid_parent_of(self, cls, obj=None):
-	        return not issubclass(cls, (MainContentArea, SidebarArea))
-
-    	@classmethod
-    	def valid_child_of(cls, parent, obj=None):
-        	return isinstance(parent, I18NLayout)
-
-    	class Meta:
-        	verbose_name = _('sidebar')
-        	verbose_name_plural = _('sidebars')
-
-
-@register
-class IndiumDefaultLayout(I18NLayout):
-    	class Meta:
-        	verbose_name = _('default layout')
-        	verbose_name_plural = _('default layouts')
-
-    	default_children = [
-        	('main', MainContentArea, (), {}),
-        	('sidebar', SidebarArea, (), {}),
-    	]
-
-@register
-class LayoutContainer(I18NLayoutContainer):
-    	child_class = IndiumDefaultLayout
-
-	class Meta:		
-        	verbose_name = _('layout container')
-        	verbose_name_plural = _('layout containers')
-
-
-def i18nreverse(language, *args, **kwargs):
-    	current_language = get_language()
-    	activate(language)
-    	url = urlresolvers.reverse(*args, **kwargs)
-    	activate(current_language)
-    	return url
-
-@links.register
-class WidgetPage(WidgyPageMixin, Page):
-	permanent_id = models.CharField(max_length=255, default="", verbose_name="permanent id")
-
-	root_node = VersionedWidgyField(
-	    site=settings.WIDGY_MEZZANINE_SITE,
-	    verbose_name=_('widgy content'),
-	    root_choices=(
-		'widgy_i18n.LayoutContainer',
-	    ),
-	    # WidgyField used to have these as defaults.
-	    null=True,
-	    on_delete=models.SET_NULL,
-	)
-
-	class Meta:
-	    	verbose_name = _('widget page')
-	    	verbose_name_plural = _('widget pages')
-
-	objects = PageSelectRelatedManager(select_related=[
-	    'root_node'
-	    'root_node__head',
-	    # we might not need this, if head isn't published, but we
-	    # probably will.
-	    'root_node__head__root_node',
-	])
-	
-	def get_action_links(self, root_node):
-		# Provide the preview links for the various languages.
-		return [
-		    {
-			'type': 'preview',
-			'text': _('Preview (%s)') % node.content.language_code,
-			'url': i18nreverse(node.content.language_code,
-			    'widgy.contrib.widgy_mezzanine.views.preview',
-			    kwargs={'slug': self.slug, 'node_pk': node.pk}
-			)
-		    }
-		    for node in root_node.get_children()
-		]
-	
-"""
