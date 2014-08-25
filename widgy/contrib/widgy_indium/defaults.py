f36db72f46d328020974e2df2a9ec8cd62c2f4c0
Johannes Ylönen <johannes@indiumgames.fi>
2014-08-25 10:36:08 +0300

    Added field for footer to be changed from admin. And some cleaning for widgy:indium templates

---
+++ widgy/contrib/widgy_indium/defaults.py
@@ -0,0 +1,42 @@
+from django.utils.translation import ugettext_lazy as _
+from mezzanine.conf import register_setting
+
+
+register_setting(
+    name="FOOTER_COMPANY",
+    description=_("The company name shown in the footer of page."),
+    editable=True,
+    default="",
+)
+register_setting(
+    name="FOOTER_ADRESS_STREET",
+    description=_("The street for address shown in the footer of page."),
+    editable=True,
+    default="",
+)
+register_setting(
+    name="FOOTER_ADRESS_POSTAL_CODE_AND_CITY",
+    description=_("The postal code and city for address shown in the footer of page."),
+    editable=True,
+    default="",
+)
+
+register_setting(
+    name="FOOTER_PHONE",
+    description=_("The phone number shown in the footer of page."),
+    editable=True,
+    default="",
+)
+register_setting(
+    name="FOOTER_EMAIL",
+    description=_("The e-mail address shown in the footer of page."),
+    editable=True,
+    default="",
+)
+
+register_setting(
+    name="FOOTER_COMPANY_ID",
+    description=_("The company id shown in the footer of page."),
+    editable=True,
+    default="",
+)
